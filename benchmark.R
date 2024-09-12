77da5aa5fbc3062f842433eed9c8ceee80c7e3cb
Lukas Burk <lukas@quantenbrot.de>
2024-09-12 20:27:46 +0200

    Pretest

--- benchmark.R
+++ benchmark.R
@@ -32,10 +32,30 @@ reg <- makeExperimentRegistry(
 )
 
 mlr3batchmark::batchmark(design, store_models = FALSE)
-tab <- merge(unwrap(getJobTable()), task_meta, by = "task_id")
+tab <- ljoin(unwrap(getJobTable()), task_meta, by = "task_id")
+data.table::setkey(tab, job.id)
 
-sample_ids = tab[dim_rank <= 10, .SD[sample(nrow(.SD), 1)], by = c("task_id", "learner_id")]
-sample_ids
+sample_ids = tab[dim_rank <= 5, .SD[sample(nrow(.SD), 1)], by = c("task_id", "learner_id")]
+# sample_ids
 
 submitJobs(sample_ids)
+waitForJobs()
 
+bmr <- mlr3batchmark::reduceResultsBatchmark(findDone(), store_backends = FALSE)
+
+measure <- msr("regr.mse", id = "mse")
+scores <- bmr$score(measure, conditions = TRUE)
+aggr <- bmr$aggregate(measure, conditions = FALSE)
+
+
+if (!fs::dir_exists(fs::path_dir(conf$result_path))) {
+  fs::dir_create(fs::path_dir(conf$result_path))
+}
+
+if (!fs::dir_exists(conf$result_path)) {
+  fs::dir_create(conf$result_path)
+}
+
+saveRDS(bmr, fs::path(conf$result_path, "bmr", ext = "rds"))
+saveRDS(scores, fs::path(conf$result_path, "scores", ext = "rds"))
+saveRDS(aggr, fs::path(conf$result_path, "aggr", ext = "rds"))
